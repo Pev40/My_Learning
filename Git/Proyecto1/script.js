8bc3c293347b9a86aee8d0e3410c98a346cc1816
Piero Vizcarra <pevv2016@gmail.com>
2021-05-10 16:54:04 -0500

    Se envio al mater este blog

--- Git/Proyecto1/script.js
+++ Git/Proyecto1/script.js
@@ -1,1 +1,9 @@
-alert("Hello");
+var nombre = document.getElementById("nombre");
+var botonazo = document.addEventListener("click","boton");
+console.log("WAWAW");
+console.log(botonazo.addEventListener("click","boton"));
+function mostrarBienvenida(){
+    if(botonazo){
+        alert("Bienvenido " + nombre);
+    }
+}
