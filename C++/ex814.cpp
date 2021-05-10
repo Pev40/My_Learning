099f0326abaad30c73e70d9078a9cd82c0f2a06c
Piero Vizcarra <pevv2016@gmail.com>
2021-05-10 14:22:30 -0500

    Se cambio el archivo ex814.cpp

--- C++/ex814.cpp
+++ C++/ex814.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    char string1;
+    cin >> string1;
+    cout << string1;
 
-int main(){
-    /*char string1;
-    cin>>string1;
-    cout<<string1;
- */
-cout<<"Hola Mundo";   
+    cout << "Hola Mundo";
 }
