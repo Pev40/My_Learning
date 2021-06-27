eb751555f7f2024480d8a7671d1af153031a5b8f
Piero Vizcarra Vargas <pevv2016@gmail.com>
2021-06-26 21:55:17 -0500

    Aprendizaje Con Java SE Variables, encapsulamiento,proximamente abstraccion

---
+++ AnalisisUberPOO/clase1.py
@@ -0,0 +1,29 @@
+#Defina  en  python  una  pila  que  almacene  la  información  de  
+#los  mensajes  de  correo  electrónico  que  recibe  una 
+#persona.  Construya  las  funciones  "agregarCorreo"  y  "leerCorreo". 
+# La  pila  debe  almacenar  los  siguientes  datos: 
+#Mensaje, remitente, hora, fecha y asunto.
+
+#PILA o LISTA
+# []  
+#TUPLA
+# () 
+
+
+Lista = [] #Pila
+
+
+
+
+def AgregarAPila(Mensaje,Remitente,Hora,Fecha,Asunto):
+
+
+
+
+
+def run():
+    print("Hello")
+
+
+if __name__ == '__main__':
+    run()
